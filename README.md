0e1c1f96d2ebb39c3452dccc78355ed986a27714
Raff Abenoja <raff@symph.co>
2024-07-03 23:33:39 +0800

    New Files Update
    Refreshed all files. With added DLCs and other new items.

---
+++ README.md
@@ -0,0 +1,21 @@
+# FurBLE - Furby Web Bluetooth Demo
+Use Web Bluetooth to control your Furby Connect! Works best for Chrome on Android
+
+## To test this out, [click here](https://raffabenoja.github.io/furble/furble.html)
+## You can also input pre-programmed [list of actions here](https://github.com/Jeija/bluefluff/blob/master/doc/actionlist.md)
+
+# IMPORTANT!
+Reset the Furby Connect every time you want to upload a new DLC. This is to avoid bricking.
+## To reset the Furby Connect:
+- Make sure the Furby is awake, and turn it upside down
+- Push its tongue and pull its tail for 10 seconds.
+- The furby's eyes will turn off. If it doesn't repeat step 1. When it does turn off, you can wake it up again.
+
+<img src="images/furby1.jpg" width="500">
+
+### Working:
+- Connect to / disconnect from Furby
+- Send basic commands (e.g. actions, set antenna colour)
+- Send custom action numbers (see [list of action sequences](https://github.com/Jeija/bluefluff/blob/master/doc/actions.md))
+- Upload and activate DLC files
+- Decode and display Furby state (antenna joystick, body sensors, orientation)
---
+++ dlc/index.js
@@ -0,0 +1,73 @@
+[
+    {
+        "file": "Orks.dlc", 
+        "title": "Orks",
+        "buttons": [
+            { "title": "Be Green", "action": [75,0,4,1] },
+            { "title": "Waaagh", "action": [75,0,3,2] },
+            { "title": "Boss", "action": [75,0,4,0] },
+            { "title": "Pee", "action": [75,0,6,5] },
+            { "title": "Listen", "action": [75,0,5,6] },
+            { "title": "Spit", "action": [75,0,6,6] },
+            { "title": "Snotling", "action": [75,0,4,5] },
+            { "title": "Top Ork", "action": [75,0,4,6] },
+            { "title": "Snivel", "action": [75,0,7,6] },
+            { "title": "Void", "action": [75,0,0,3] },
+            { "title": "Orks", "action": [75,0,0,2] },
+            { "title": "Green iz best", "action": [75,0,4,7] },
+            { "title": "In Charge", "action": [75,0,3,3] } 
+        ]
+    },
+    {
+        "file": "Waaagh.dlc", 
+        "title": "Waaaagh",
+        "buttons": [
+            { "title": "WAAAAAAGH!", "action": [75,0,0,2] }
+        ]
+    },
+    
+    {
+        "file": "EvilSeed.dlc", 
+        "title": "Evil Seed",
+        "buttons": [
+            { "title": "What have you done?", "action": [75,0,0,2] }
+        ]
+    },
+    { 
+        "file": "SciFix.dlc", 
+        "title": "Science!",
+        "buttons": [
+            { "title": "FUCK MY HOUSE!", "action": [75,0,0,2] }
+         ]
+    },
+    {
+        "file": "chilli.dlc", 
+        "title": "Chilli",
+        "buttons": [
+            { "title": "Chilli ", "action": [75,0,4,4] }
+        ]
+    },
+    
+    { 
+        "file": "context.dlc", 
+        "title": "Context Logo",
+        "buttons": [
+            { "title": "Logo", "action": [75,0,4,4] }
+        ]
+    },
+    { 
+        "file": "hacked.dlc", 
+        "title": "HACKED",
+        "buttons": [
+            { "title": "Exterminate", "action": [75,0,3,4] },
+            { "title": "Kill all Humans", "action": [75,0,4,4] }
+        ]
+    },
+    {
+        "file": "noimg2.dlc", 
+        "title": "Audio only",
+        "buttons": [
+            { "title": "Audio test ", "action": [75,0,4,4] }
+        ]
+    }
+]
