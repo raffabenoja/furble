5a65261bbc044f6045f3190b8e3acdfce23b91e0
Raff Abenoja <raffabenoja@gmail.com>
2024-07-08 07:00:42 +0800

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -9,25 +9,13 @@
             { "title": "Pee", "action": [75,0,6,5] },
             { "title": "Listen", "action": [75,0,5,6] },
             { "title": "Spit", "action": [75,0,6,6] },
-            { "title": "Snortling", "action": [75,0,4,5] },
+            { "title": "Snotling", "action": [75,0,4,5] },
             { "title": "Top Ork", "action": [75,0,4,6] },
             { "title": "Snivel", "action": [75,0,7,6] },
             { "title": "Void", "action": [75,0,0,3] },
             { "title": "Orks", "action": [75,0,0,2] },
             { "title": "Green iz best", "action": [75,0,4,7] },
             { "title": "In Charge", "action": [75,0,3,3] } 
-        ]
-    },
-    {
-        "file": "tu011090.dlc", 
-        "title": "Song Pack 1",
-        "buttons": [
-            { "title": "WeDontTalk1", "action": [75,0,2,1] },
-            { "title": "WeDontTalk2", "action": [75,0,2,3] },
-            { "title": "PartyRock1", "action": [75,0,1,1] },
-            { "title": "PartyRock2", "action": [75,0,1,3] },
-            { "title": "BoyWeightLifter", "action": [75,0,3,1] },
-            
         ]
     },
     {
@@ -42,7 +30,7 @@
         "file": "EvilSeed.dlc", 
         "title": "Evil Seed",
         "buttons": [
-            { "title": "Very Long Song", "action": [75,0,0,2] }
+            { "title": "What have you done?", "action": [75,0,0,2] }
         ]
     },
     { 
@@ -69,7 +57,7 @@
     },
     { 
         "file": "hacked.dlc", 
-        "title": "Daleks!",
+        "title": "HACKED",
         "buttons": [
             { "title": "Exterminate", "action": [75,0,3,4] },
             { "title": "Kill all Humans", "action": [75,0,4,4] }
@@ -81,5 +69,5 @@
         "buttons": [
             { "title": "Audio test ", "action": [75,0,4,4] }
         ]
-    },
+    }
 ]
