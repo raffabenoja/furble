0f6d63389fa17ac01169103d3dda41e9ac2d4b8f
Raff Abenoja <raffabenoja@gmail.com>
2024-07-08 08:27:49 +0800

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -1,11 +1,86 @@
 [
     {
         "file": "tu011090.dlc", 
-        "title": "Songs Pack 1",
+        "title": "Dont Talk / Part Rock",
         "buttons": [
-            { "title": "wedonttalk", "action": [75,0,2,3] },
-            { "title": "partyrock", "action": [75,0,1,1] },
-            { "title": "boylifter", "action": [75,0,3,1] }
+            { "title": "wedonttalk1", "action": [75,0,2,1] },
+            { "title": "wedonttalk2", "action": [75,0,2,2] },
+            { "title": "wedonttalk3", "action": [75,0,2,3] },
+            { "title": "wedonttalk4", "action": [75,0,2,4] },
+            { "title": "wedonttalk5", "action": [75,0,2,5] },
+            { "title": "wedonttalk6", "action": [75,0,2,6] },
+            { "title": "wedonttalk7", "action": [75,0,2,7] },
+            { "title": "partyrock1", "action": [75,0,1,1] },
+            { "title": "partyrock2", "action": [75,0,1,2] },
+            { "title": "partyrock3", "action": [75,0,1,3] },
+            { "title": "partyrock4", "action": [75,0,1,4] },
+            { "title": "partyrock5", "action": [75,0,1,5] },
+            { "title": "partyrock6", "action": [75,0,1,6] },
+            { "title": "partyrock7", "action": [75,0,1,7] },
+            { "title": "boylifter1", "action": [75,0,3,1] }
+            { "title": "boylifter2", "action": [75,0,3,2] }
+            { "title": "boylifter3", "action": [75,0,3,3] }
+            { "title": "boylifter4", "action": [75,0,3,4] }
+            { "title": "boylifter5", "action": [75,0,3,5] }
+            { "title": "boylifter6", "action": [75,0,3,6] }
+            { "title": "boylifter7", "action": [75,0,3,7] }
+        ]
+    },
+    {
+        "file": "tu012750.dlc", 
+        "title": "Cant Touch / Jingle Bell Rock",
+        "buttons": [
+            { "title": "canttouchthis1", "action": [75,0,1,1] },
+            { "title": "canttouchthis2", "action": [75,0,1,2] },
+            { "title": "canttouchthis3", "action": [75,0,1,3] },
+            { "title": "canttouchthis4", "action": [75,0,1,4] },
+            { "title": "canttouchthis5", "action": [75,0,1,5] },
+            { "title": "canttouchthis6", "action": [75,0,1,6] },
+            { "title": "canttouchthis7", "action": [75,0,1,7] },
+            { "title": "jinglebellrock1", "action": [75,0,2,1] },
+            { "title": "jinglebellrock2", "action": [75,0,2,2] },
+            { "title": "jinglebellrock3", "action": [75,0,2,3] },
+            { "title": "jinglebellrock4", "action": [75,0,2,4] },
+            { "title": "jinglebellrock5", "action": [75,0,2,5] },
+            { "title": "jinglebellrock6", "action": [75,0,2,6] },
+            { "title": "jinglebellrock7", "action": [75,0,2,7] }
+        ]
+    },
+    {
+        "file": "tu012770.dlc", 
+        "title": "Still Into You / Call Me Maybe",
+        "buttons": [
+            { "title": "stillintoyou1", "action": [75,0,1,1] },
+            { "title": "stillintoyou2", "action": [75,0,1,2] },
+            { "title": "stillintoyou3", "action": [75,0,1,3] },
+            { "title": "stillintoyou4", "action": [75,0,1,4] },
+            { "title": "stillintoyou5", "action": [75,0,1,5] },
+            { "title": "stillintoyou6", "action": [75,0,1,6] },
+            { "title": "stillintoyou7", "action": [75,0,1,7] },
+            
+            { "title": "callmemaybe1", "action": [75,0,2,1] },
+            { "title": "callmemaybe2", "action": [75,0,2,2] },
+            { "title": "callmemaybe3", "action": [75,0,2,3] },
+            { "title": "callmemaybe4", "action": [75,0,2,4] },
+            { "title": "callmemaybe5", "action": [75,0,2,5] },
+            { "title": "callmemaybe6", "action": [75,0,2,6] },
+            { "title": "callmemaybe7", "action": [75,0,2,7] },
+
+            { "title": "gangnam1", "action": [75,0,3,1] },
+            { "title": "gangnam2", "action": [75,0,3,2] },
+            { "title": "gangnam3", "action": [75,0,3,3] },
+            { "title": "gangnam4", "action": [75,0,3,4] },
+            { "title": "gangnam5", "action": [75,0,3,5] },
+            { "title": "gangnam6", "action": [75,0,3,6] },
+            { "title": "gangnam7", "action": [75,0,3,7] },
+
+            { "title": "riptide1", "action": [75,0,4,1] },
+            { "title": "riptide2", "action": [75,0,4,2] },
+            { "title": "riptide3", "action": [75,0,4,3] },
+            { "title": "riptide4", "action": [75,0,4,4] },
+            { "title": "riptide5", "action": [75,0,4,5] },
+            { "title": "riptide6", "action": [75,0,4,6] },
+            { "title": "riptide7", "action": [75,0,4,7] }
         ]
     },
     {
